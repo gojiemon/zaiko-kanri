51b0d4ec2eb0de1d030858cfe6a93dc16b8dd2e5
gojiemon <uujimoto@gmail.com>
2025-10-27 13:26:22 +0900

    set GAS url

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 // 例）const GAS_API_BASE = "https://script.google.com/macros/s/AKfycbx.../exec";
 // 後でGoogle Apps ScriptのWebアプリURLを設定してください。
-const GAS_API_BASE = ""; // ユーザーが後で設定
+const GAS_API_BASE = "hhttps://script.google.com/macros/library/d/1x2_LrrpVZJ-GwV6sdo0vw2N8b7fJMSn0O1qgzR7KMS3yRGVCbFJAqr92/1"; // ユーザーが後で設定
 
